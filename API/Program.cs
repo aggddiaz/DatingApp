11fcf6cb5e5909a8421b8ec09357c2a9a70a8533
Alina Garcia <agarcia001@ilshealth.net>
2024-02-21 11:27:33 -0500

    Commit 02_21_2024

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,6 @@
 using API.Data;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +12,13 @@ builder.Services.AddDbContext<DataContext>( opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
