c14c80d45544a3bf57a3da0ee0a1e0a7ecbb8678
Alina Garcia <agarcia001@ilshealth.net>
2024-02-22 12:55:41 -0500

    Commit_02_22_2024

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import {HttpClient} from '@angular/common/http'
 import { Component, OnInit } from '@angular/core';
+//import { NgFor } from '@angular/common'; 
+
+import { CommonModule } from '@angular/common';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @Component({
   selector: 'app-root', 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [ CommonModule],
 })
 
 export class AppComponent implements OnInit {  
@@ -13,7 +19,7 @@ export class AppComponent implements OnInit {
   users: any;
   constructor(private http: HttpClient) {}
   ngOnInit():void{
-    this.http.get('https://localhost:5241/api/users').subscribe({
+    this.http.get('http://localhost:5241/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete : () => console.log('Request has completed')
